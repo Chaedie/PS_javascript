c98d6b746fef76d5ec9da95a02ed800a58497294
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-06-06 18:28:46 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,19 @@
+//* 시스템
+function solution() {
+  // let answer = a - b;
+  let answer = '\\    /\\\n' + " )  ( ')\n" + '(  /  )\n' + ' \\(__)|';
+  console.log(answer);
+}
+
+//* 인풋
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString();
+
+let nums = input.split(' ');
+
+let a = Number(nums[0]);
+let b = Number(nums[1]);
+
+//*아웃풋
+solution();
