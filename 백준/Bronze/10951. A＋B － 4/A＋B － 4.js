01cb7f7133f704b8cae621018cdae186505a641b
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-06-09 14:18:03 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,23 @@
+//* 인풋 - 디폴트
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+// const n = Number(input[0]);
+
+for (let i = 0; i < input.length; i++) {
+  let nums = input[i]
+    .trim()
+    .split(' ')
+    .map((num) => Number(num));
+  solution(nums);
+}
+
+//* 시스템
+function solution(nums) {
+  let sum = nums[0] + nums[1];
+  if (!isNaN(sum)) {
+    console.log(sum);
+  }
+}
