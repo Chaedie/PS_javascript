de917fa9dbaa1385ba8c98363ecb1b4c5a8387d7
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-06-15 11:16:46 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -6,38 +6,26 @@ const input = fs.readFileSync(filePath).toString().split('\n');
 //* 인풋 - 커스텀
 const n = Number(input[0]);
 
-console.log(solution());
-
 //* 시스템
-function solution() {
-  let answer = '';
-  let strs = [];
-  let value;
-
-  for (let i = 1; i <= n; i++) {
-    strs = input[i].trim();
-    answer += isVPS(strs) + '\n';
-  }
-
-  return answer;
+let answer = '';
+for (let i = 1; i <= n; i++) {
+  let strs = input[i].trim();
+  answer += isVPS(strs) + '\n';
 }
 
+console.log(answer);
+
 function isVPS(strs) {
-  let answer = '';
   let stack = [];
 
   for (let i = 0; i < strs.length; i++) {
     if (stack.length === 0 && strs[i] === ')') {
       return 'NO';
     }
-    if (stack.length === 0) {
+    if (strs[i] === '(') {
       stack.push(strs[i]);
     } else {
-      if (strs[i] === '(') {
-        stack.push(strs[i]);
-      } else {
-        stack.pop();
-      }
+      stack.pop();
     }
   }
 
