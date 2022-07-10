70091a41c54a6b0709d0f5d87df29e177ec9dc59
ChaedongIm <chaedong.im.dev@gmail.com>
2022-07-10 09:37:45 +0900

    [Bronze II] Title: 대표값2, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.js
+++ 백준/Bronze/2587. 대표값2/대표값2.js
@@ -2,6 +2,8 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 //* 인풋 (커스텀)
+// const n = +input[0];
+// const nums = input[0].split(' ').map((x) => +x);
 const nums = input.map((x) => +x);
 
 //* 함수 콜 (고정)
@@ -11,15 +13,7 @@ console.log(typeof answer === 'number' ? answer : answer.trimEnd());
 //* 로직함수
 function solution() {
   let answer = [];
-  answer.push(getAvg(nums));
-  answer.push(getMedium(nums));
+  answer.push(nums.reduce((acc, cur) => (acc += cur), 0) / nums.length);
+  answer.push(nums.sort((a, b) => a - b)[Math.floor(nums.length / 2)]);
   return answer.join('\n');
-}
-
-function getAvg(nums) {
-  return nums.reduce((acc, cur) => (acc += cur), 0) / nums.length;
-}
-
-function getMedium(nums) {
-  return nums.sort((a, b) => a - b)[Math.floor(nums.length / 2)];
 }
